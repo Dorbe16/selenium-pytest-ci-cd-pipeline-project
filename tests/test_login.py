3adaea1b377bdaca8a43e665579a5df8a0b3991a
Dorbe16 <alexdbr@yahoo.com>
2025-10-12 12:59:53 +0300

    modified script

--- tests/test_login.py
+++ tests/test_login.py
@@ -21,7 +21,19 @@ def test_logout(driver):
     page.open()
     time.sleep(2)
     page.login("tomsmith", "SuperSecretPassword!")
-    time.sleep(2)
+    time.sleep(3)
+    login_message = page.get_flash_message()
+    print(f"Message after login: {login_message}")
     page.logout()
-    time.sleep(2)
-    assert "You logged out of the secure area!" in page.get_flash_message()
+
+    for i in range(10):
+        time.sleep(1)
+        curr_message = page.get_flash_message()
+        print(f"Attempt {i+1}: {curr_message}")
+        if "logged out" in curr_message.lower():
+            break
+        final_message = page.get_flash_message()
+        print(f"Final message: {final_message}")
+        assert "You logged out of the secure area!" in final_message
+    #time.sleep(2)
+    #assert "You logged out of the secure area!" in page.get_flash_message()
